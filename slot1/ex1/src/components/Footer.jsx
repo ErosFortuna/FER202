f856a7c0f30dfb109172be96e5881673fee5f6a4
ErosFortuna <hoangquoc316@gmail.com>
2026-01-07 09:27:04 +0200

    fix: remove nested git repo

---
+++ slot1/ex1/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+        <>
+            <h1>
+                day la web react cua toi
+            </h1>
+            <p>
+                lien he abc@gmai.com
+            </p>
+            <img src="/image/4c9802d4bbefa7f729af60eb39bf6d52.jpg" alt="none" height={100} width={100
+            } />
+        </>
+    )
+}
+export default Footer;
