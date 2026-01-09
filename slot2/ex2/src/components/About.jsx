330277abf9cce998c5b930ddc85f84b4ccda63ae
ErosFortuna <hoangquoc316@gmail.com>
2026-01-09 17:03:47 +0700

    them slot 2

--- slot2/ex2/src/components/About.jsx
+++ slot2/ex2/src/components/About.jsx
@@ -1,6 +1,7 @@
 //khai bao 1 doi tuong gom id,name,avatar,grade
 //in thongg tin cua h1,pvaf img
-import { Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 
@@ -16,23 +17,18 @@ function About() {
     };
     console.log(student);
     return (
-     <div className="container mt-4">
-      <div className="card" style={{ width: "18rem" }}>
-        <img
-          src={student.avatar}
-          className="card-img-top"
-          alt="avatar"
-        />
+   <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={student.avatar} />
 
-        <div className="card-body">
-          <h5 className="card-title">{student.name}</h5>
-          <p className="card-text">ID: {student.id}</p>
-          <p className="card-text">Age: {student.age}</p>
-          <p className="card-text">Grade: {student.grade}</p>
-        </div>
-        <Button variant="primary">Go somewhere</Button>
-      </div>
-    </div>
+      <Card.Body>
+        <Card.Title>{student.name}</Card.Title>
+        <Card.Text>ID: {student.id}</Card.Text>
+        <Card.Text>Age: {student.age}</Card.Text>
+        <Card.Text>Grade: {student.grade}</Card.Text>
+
+        <Button variant="primary">View Profile</Button>
+      </Card.Body>
+    </Card>
     );
 
     }
