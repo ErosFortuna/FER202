5abbbdd94143588e10768fa2f41da3caa574096e
ErosFortuna <hoangquoc316@gmail.com>
2026-01-14 09:09:34 +0200

    slot 3 lab 1

---
+++ slot3/lab1/src/data/studentData.js
@@ -0,0 +1,73 @@
+export const studentsData = [
+  {
+    id: 1,
+    name: "Angelina Jolie",
+    avatar: "/images/images.webp",
+    age: 49,
+    grade: "A"
+  },
+  {
+    id: 2,
+    name: "Scarlett Johansson",
+    avatar: "/images/licensed-image.webp",
+    age: 40,
+    grade: "A"
+  },
+  {
+    id: 3,
+    name: "Gal Gadot",
+    avatar: "/images/images (1).webp",
+    age: 39,
+    grade: "A"
+  },
+  {
+    id: 4,
+    name: "Emma Watson",
+    avatar: "/images/images (2).webp",
+    age: 34,
+    grade: "A"
+  },
+  {
+    id: 5,
+    name: "Zendaya",
+    avatar: "/images/images (3).webp",
+    age: 28,
+    grade: "A"
+  },
+
+  {
+    id: 6,
+    name: "Margot Robbie",
+    avatar: "/images/Margot Robbie.webp",
+    age: 34,
+    grade: "A"
+  },
+  {
+    id: 7,
+    name: "Ana de Armas",
+    avatar: "/images/Ana de Armas.webp",
+    age: 36,
+    grade: "A"
+  },
+  {
+    id: 8,
+    name: "Emma Stone",
+    avatar: "/images/Emma Stone.webp",
+    age: 36,
+    grade: "A"
+  },
+  {
+    id: 9,
+    name: "Natalie Portman",
+    avatar: "/images/Natalie Portman.webp",
+    age: 43,
+    grade: "A"
+  },
+  {
+    id: 10,
+    name: "Taylor Swift",
+    avatar: "/images/Taylor Swift.webp",
+    age: 35,
+    grade: "A"
+  }
+];
